4906cd77f21e6581f4c525e0b5c26d4b44f030f4
Grégoire Gaonach <gregoire.gaonach@isen.yncrea.fr>
2017-06-12 15:25:26 +0200

    Add files via upload
    Premier upload

---
+++ Scripts/index.py
@@ -0,0 +1,33 @@
+﻿import Constellation
+import twitter_lib as Tw
+import json
+import time
+
+def OnStart():
+
+    # Récupération des informations de configuration (clé Twitter)
+    if Constellation.GetSetting("twitter_access_token_key") <> None:
+        Constellation.WriteInfo("Clés de configurations récupérées : (%s ; %s)" % (Constellation.GetSetting("twitter_access_token_key"), Constellation.GetSetting("twitter_access_token_secret")))
+    else:
+        #Constellation.RequestSettings()
+        Constellation.WriteError("Problèm lors de la configuration du paquet")
+
+    while(True):
+        # Informations basiques liées à un compte Twitter :
+        basicInformations   = Tw.basicInformations(Constellation.GetSetting("twitter_access_token_key"), Constellation.GetSetting("twitter_access_token_secret"))
+
+        # Vérification de la connexion au serveur Twitter
+        try:
+            basicInformations.id
+        except error:
+            Constellation.WriteError("Impossible de se connecter au serveur Twitter...")
+        else:
+            # Push des données
+            Constellation.WriteInfo("Connexion au serveur Twitter : récupération des informations de @%s terminée !" % basicInformations.screen_name)
+            Constellation.PushStateObject(basicInformations.screen_name, basicInformations.AsDict(), lifetime = 30)
+
+        time.sleep(30)
+
+    pass
+
+Constellation.Start(OnStart);
---
+++ bin/Debug/Scripts/twitter_lib.py
@@ -0,0 +1,24 @@
+﻿import Constellation
+import twitter
+
+""" FONCTION : basicInformations
+    ============================
+    Permet de récupérer les informations basiques à l'aide de l'API Twitter
+    et la dépendance associée python-twitter
+
+    Voir pour référence :
+    API Twitter - https://apps.twitter.com/
+    Python-Twitter - https://github.com/bear/python-twitter
+
+    PARAMETRES :
+    ============
+    access_token_key : jeu de clefs associé à un compte Twitter via oAuth
+    access_token_secret : jeu de clefs associé à un compte Twitter via oAuth
+"""
+def basicInformations(atk, ats):
+    api = twitter.Api(consumer_key        = 'ygOLcfUomXDvSUfpinYvXg4Zz',                
+                      consumer_secret     = 'OsOHpzzZTNIbzztnr1nmltmx2mg1iVcFi4eygNEpcEWB2lCEtv', 
+                      access_token_key    = atk, 
+                      access_token_secret = ats) 
+
+    return api.VerifyCredentials()
